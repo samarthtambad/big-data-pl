0727478dd7853a9c7324f56ec0c9c08fc9a75014
samarthtambad <tambad.samarth@gmail.com>
2020-05-01 16:39:40 -0400

    refactored into classes to remove err

--- src/main/scala/app_code/analytics.scala
+++ src/main/scala/app_code/analytics.scala
@@ -1,22 +1,22 @@
-/* 
-Analytics on data to generate insights
+package analytics
 
-Assume following DFs already loaded in interactive mode
+class Analyze(var path: String) {
+    /* 
+    Analytics on data to generate insights
 
-usersDF
-commitsDF
-pullRequestsDF
-projectsDF
-projectLanguagesDF
-*/
+    Assume following DFs already loaded in interactive mode
 
-val joinedCommitsLangData = commitsDF.join(projectLanguagesDF, Seq("project_id"), "inner")
-val df_countCommitLangData = sqlContext.sql("""SELECT year, COUNT(language) FROM joinedCommitsLangData GROUP BY year""")
-val df_countProjectLang = sqlContext.sql("""SELECT year, COUNT(language) FROM projectLanguagesData GROUP BY year""")
+    usersDF
+    commitsDF
+    pullRequestsDF
+    projectsDF
+    projectLanguagesDF
+    */
 
+    val joinedCommitsLangData = commitsDF.join(projectLanguagesDF, Seq("project_id"), "inner")
+    val df_countCommitLangData = sqlContext.sql("""SELECT year, COUNT(language) FROM joinedCommitsLangData GROUP BY year""")
+    val df_countProjectLang = sqlContext.sql("""SELECT year, COUNT(language) FROM projectLanguagesData GROUP BY year""")
 
-
-// 2D aggregation - num projects per language per year
-val numProjects = projectsDF.rollup("year", "language").agg(count("language") as "count").sort($"count".desc)
-
-
+    // 2D aggregation - num projects per language per year
+    val numProjects = projectsDF.rollup("year", "language").agg(count("language") as "count").sort($"count".desc)
+}
