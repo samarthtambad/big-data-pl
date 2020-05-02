1473365f2c68a55c654e3682932ab4f25be11853
samarthtambad <tambad.samarth@gmail.com>
2020-05-02 18:52:19 -0400

    analysis code added

--- src/main/scala/app_code/analysis_github.scala
+++ src/main/scala/app_code/analysis_github.scala
@@ -61,7 +61,7 @@ object AnalyzeGithub {
 
     // get number of projects by language per year
     private def computeNumProjects(spark: SparkSession, df: DataFrame): Unit = {
-        val numProjectsDF = df.groupBy("year", "language").agg(count("language") as "count").sort(desc("count"))
+        val numProjectsDF = df.groupBy("year", "language").agg(count("language") as "num_projects").sort(desc("num_projects"))
         numProjectsDF.coalesce(1).write.format("csv").mode("overwrite").option("header", "true").save(baseSavePath + "time_num_projects.csv")
     }
 
